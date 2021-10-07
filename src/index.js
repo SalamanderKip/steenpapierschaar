704adeb83a0cc16f83f504388a53c4e253a0f5ef
SalamanderKipp <200507@student.glu.nl>
2021-10-07 23:45:33 +0200

    helemaal af

--- src/index.js
+++ src/index.js
@@ -1,134 +1,138 @@
 import "./index.scss";
+let elementTimer = document.getElementById("timer")
+let elementResult = document.getElementById("result")
+let elementPlayer1Choice = document.getElementById("player1Choice")
+let elementPlayer2Choice = document.getElementById("player2Choice")
 
-class game {
-    constructor() {
+const ROCK = "1"
+const PAPER = "2"
+const SCISSORS = "3"
 
-        let elmentTimer = document.getElementById("timer")
-        let elementPlayer1 = document.getElementById("player1")
-        let elementPlayer2 = document.getElementById("player2")
-        let elementWinner = document.getElementById("winner")
+elementTimer.innerHTML = 0
+elementResult.innerHTML = "Press space to start"
+let player1 = ""
+let player2 = ""
+let timer = 5
+let readyToChoose = false;
 
-        elmentTimer.innerHTML = 0
-        elementPlayer1.innerHTML = "-"
-        elementPlayer2.innerHTML = "-"
-        elementWinner.innerHTML = "Get ready to start"
-
-        let player1 = ""
-        let player2 = ""
-        let timer = 5
-        let readyToChoose = false;
-
-        function winner() {
-            // 1/8 steen 2/9 papier 3/0 schaar
-            if ((player1 == "") && (player2 == "")) {
-                return "tie"
-            }
-            if (player1 == "") {
-                return "player2 won"
-            }
-            if (player2 == "") {
-                return "player 1 won"
-            }
-            if ((player1 == "1") && (player2 == "8")) {
-                return "tie"
-            }
-            if ((player1 == "2") && (player2 == "9")) {
-                return "player2 won"
-            }
-            if ((player1 == "2") && (player2 == "0")) {
-                return "player1 won"
-            }
-            if ((player1 == "3") && (player2 == "8")) {
-                return "player1 won"
-            }
-            if ((player1 == "3") && (player2 == "9")) {
-                return "tie"
-            }
-            if ((player1 == "3") && (player2 == "0")) {
-                return "player2 won"
-            }
-            if ((player1 == "3") && (player2 == "8")) {
-                return "player2 won"
-            }
-            if ((player1 == "3") && (player2 == "9")) {
-                return "player1 won"
-            }
-            if ((player1 == "3") && (player2 == "0")) {
-                return "tie"
-            }
-        }
-
-        function countDown() {
-            readyToChoose = false
-            if (timer == 0) {
-                gameStart()
-            }
-            else {
-                timer = timer - 1;
-                elementTimer.innerHTML = timer
-                setTimeout(countDown, 500)
-            }
-        }
-
-        function gameStart() {
-            readyToChoose = true
-            timer = 3
-            elementWinner.innerHTML = "Game started"
-            gameCountDown()
-        }
-
-        function gameCountDown() {
-            if (timer == 0) {
-                readyToChoose = false
-                elementWinner.innerHTML = winner()
-                elementPlayer1.innerHTML = player1
-                elementPlayer2.innerHTML = player2
-            } else {
-                timer = timer - 1
-                elementTimer.innerHTML = timer
-                setTimeout(gameCountDown, 500)
-
-            }
-        }
-        console.log("test");
-        document.addEventListener('keydown', (event) => {
-            console.log(event.key);
-            switch (event.key) {
-                case ' ':
-                    countDown()
-                    break;
-                case '1':
-                    player1 = "1"
-                    break;
-                case '2':
-                    player1 = "2"
-                    break;
-                case '3':
-                    player1 = "3"
-                    break;
-                case '8':
-                    player2 = "8"
-                    break;
-                case '9':
-                    player2 = "9"
-                    break;
-                case '0':
-                    player2 = "0"
-                    break;
-                default:
-                    console.log(`key: ${event.key} was not found`)
-                    break;
-            }
-        })
-
-        this.listnersadd()
+function Result() {
+    // 1/8 ROCK 2/9 PAPER 3/0 SCISSORS
+    if ((player1 == "") && (player2 == "")) {
+        return "tie"
     }
-    listnersadd() {
-        
+    if (player1 == "") {
+        return "player2 won"
+    }
+    if (player2 == "") {
+        return "player 1 won"
+    }
+    if ((player1 == ROCK) && (player2 == ROCK)) {
+        return "tie"
+    }
+    if ((player1 == SCISSORS) && (player2 == SCISSORS)) {
+        return "tie"
+    }
+    if ((player1 == PAPER) && (player2 == PAPER)) {
+        return "tie"
     }
 
+    if ((player1 == PAPER) && (player2 == SCISSORS)) {
+        return "player2 won"
+    }
+    if ((player1 == PAPER) && (player2 == ROCK)) {
+        return "player1 won"
+    }
 
+    if ((player1 == SCISSORS) && (player2 == ROCK)) {
+        return "player2 won"
+    }
+    if ((player1 == SCISSORS) && (player2 == PAPER)) {
+        return "player1 won"
+    }
+
+    if ((player1 == ROCK) && (player2 == PAPER)) {
+        return "player2 won"
+    }
+    if ((player1 == ROCK) && (player2 == SCISSORS)) {
+        return "player1 won"
+    }
+
+    return "unknown state"
 }
 
+function countDown() {
+    readyToChoose = false
+    if (timer == 0) {
+        gameStart()
+    } else {
+        timer = timer - 1
+        elementTimer.innerHTML = timer
+        setTimeout(countDown, 500)
+    }
+}
 
-const a = new game()
+function gameStart() {
+    readyToChoose = true
+    timer = 10
+    elementResult.innerHTML = "Game started"
+    gameCountDown()
+}
+
+function getChoice(choice) {
+    switch (choice) {
+        case ROCK:
+            return "ROCK";
+        case PAPER:
+            return "PAPER";
+        case SCISSORS:
+            return "SCISSORS";
+                
+        default:
+            break;
+    }
+}
+function gameCountDown() {
+    if (timer == 0) {
+        readyToChoose = false
+        elementResult.innerHTML = Result()
+        elementPlayer1Choice.innerHTML = getChoice(player1)
+        elementPlayer2Choice.innerHTML = getChoice(player2)
+    } else {
+        timer = timer - 1
+        elementTimer.innerHTML = timer
+        setTimeout(gameCountDown, 500)
+
+    }
+}
+
+document.addEventListener('keydown', (event) => {
+    console.log(event.key);
+    switch (event.key) {
+        case ' ':
+            countDown()
+            break;
+        case '1':
+            player1 = ROCK
+            break;
+        case '2':
+            player1 = PAPER
+            break;
+        case '3':
+            player1 = SCISSORS
+            break;
+        case '8':
+            player2 = ROCK
+            break;
+        case '9':
+            player2 = PAPER
+            break;
+        case '0':
+            player2 = SCISSORS
+            break;
+        default:
+            console.log(`key: ${event.key} was not found`)
+            break;
+    }
+})
+
+console.log("test");
